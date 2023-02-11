29244aaf458c3c7c96eaa82e48fd2a711c223946
howen <howenolan30@gmail.com>
2023-02-10 23:38:20 -0600

    Nothing interesting just messing with loops a little

--- src/Loop.java
+++ src/Loop.java
@@ -4,6 +4,15 @@ public class Loop {
 
     ArrayList<Integer> ar = new ArrayList<>();
     ArrayList<Integer> ar2 = new ArrayList<>();
+
+    public void addArrayValue(){
+        for(int i = 0; i < 11; i++){
+            ar.add(i,4);
+        }
+        for(int i = 0; i < 10; i++){
+            ar2.add(i,4);
+        }
+    }
     public void firstLoop(){
         int cacti = 1000;
         int count = 0;
@@ -15,12 +24,7 @@ public class Loop {
     }
 
     public void secondLoop(){
-        for(int i = 0; i < 11; i++){
-            ar.add(i,4);
-        }
-        for(int i = 0; i < 10; i++){
-            ar2.add(i,4);
-        }
+        addArrayValue();
         int count = 0;
         for(int i =0; i < ar.size(); i++) {
             for (int j = ar.size(); j < ar2.size(); j++) {
@@ -33,6 +37,18 @@ public class Loop {
     }
 
     public void thirdLoop(){
+        addArrayValue();
+        int i =0;
+        int j =0;
+        while(i<ar2.size()){
+            j = 0;
+            System.out.println("| ");
+            i++;
+            while(j<ar.size()){
+                j++;
 
+                System.out.print("| ");
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Loop loop = new Loop();
-        loop.secondLoop();
+        loop.thirdLoop();
     }
 }
